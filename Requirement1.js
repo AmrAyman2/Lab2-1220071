cf38f37442398969684efd7e78127c551e9410ef
Amr Ayman <amr.mohamed05@eng-st.cu.edu.eg>
2025-02-16 18:46:50 +0200

    done

--- Requirement1.js
+++ Requirement1.js
@@ -1,43 +1,114 @@
-var i = [], t = [], c = [], f = {};
+var inventory = [], transactions = [], categories = [], cusomFields = {};
 
-function doStuff(a, b) {
-    if (["add", "edit", "rmI"].includes(a)) {
-        if (a === "add") {
-            var itm = { n: b[0], cat: b[1], qty: b[2], prc: b[3], unt: b[4], added: new Date(), custF: b[5] || {} };
-            i.push(itm);
-            if (!c.includes(b[1])) c.push(b[1]);
-            t.push({ type: "add", itm });
-        } else if (a === "edit" && i[b[0]]) {
-            t.push({ type: "edit", old: i[b[0]], new: b.slice(1) });
-            i[b[0]] = { ...i[b[0]], n: b[1], cat: b[2], qty: b[3], prc: b[4], unt: b[5], custF: b[6] || {} };
-        } else if (a === "rmI" && i[b[0]]) {
-            t.push({ type: "delete", itm: i[b[0]] });
-            i.splice(b[0], 1);
-        }
-        console.log("=== Dashboard ===\nItems: " + i.length + "\nTotal: $" + i.reduce((tot, x) => tot + x.qty * x.prc, 0).toFixed(2) + "\nCats: " + c.join(', '));
-    }
-    if (["Sale", "rstck"].includes(a)) {
-        for (let k of i) {
-            if (k.n === b[0]) {
-                if (a === "Sale" && k.qty >= b[1]) {
-                    k.qty -= b[1];
-                    t.push({ type: "sale", itm: k, qtyS: b[1], d: new Date() });
-                    console.log(`Sold ${b[1]} ${k.unt} of ${k.n}`);
-                } else if (a === "rstck") {
-                    k.qty += b[1];
-                    t.push({ type: "restock", itm: k, qtyR: b[1], d: new Date() });
-                    console.log(`Restocked ${b[1]} ${k.unt} of ${k.n}`);
+function makeActions(action, item) {
+
+    switch(action){
+        case "add":
+            addItem(item);
+            break;
+
+        case "edit":
+            editFirstItem(item);
+            break;
+
+        case "remove":
+            removeFirstItem(item);
+            break;
+
+        case "sell":
+            for (let k of inventory) {
+                if (k.name === item[0]) {
+                    if (k.quantity >= item[1]) {
+                        sellItem(item);
+                    break;
                 }
-                break;
             }
         }
+            break;
+
+        case "restock":
+            for (let k of inventory) {
+                if (k.name === item[0]) {
+                        restockItem(item);
+                    }
+                    break;
+            }
+            break;
     }
-    if (a === "srch") console.log(i.filter(x => [x.n, x.cat, x.prc].some(v => v.toString().toLowerCase().includes(b[0].toLowerCase()))));
-    if (a === "vwI") console.log("=== Inv ===", i);
-    if (a === "xprtAll") console.log("CSV:\n" + ["Name,Category,Quantity,Price,Unit,AddedAt"].concat(i.map(x => Object.values(x).join(','))).join('\n'));
-    if (a === "vwAllT") console.log("Transactions:\n", t);
-    if (a === "vwIAg") console.log(i.map(x => `${x.n}: ${Math.floor((new Date() - new Date(x.added)) / (1000 * 60 * 60 * 24))}d`).join('\n'));
-    if (a === "Imprt") b[0].forEach(x => doStuff("add", [x.n, x.cat, x.quantity, x.price, x.unit]));
-    if (a === "addFld" && !f[b[0]]) f[b[0]] = null;
-    if (a === "udCFld") i.find(x => x.n === b[0])?.custF[b[1]] = b[2];
+
+
+    if (action === "search"){
+        console.log(inventory.filter(item => [item.name, item.category, item.price]
+        .some(v => v.toString().toLowerCase().includes(item[0].toLowerCase()))));
+    } 
+
+    if (action === "viewInventory"){
+        console.log("=== Inventory1 ===", inventory);
+    } 
+
+    //export all items in a csv format
+    if (action === "exportAll"){
+        console.log("CSV:\name" + ["Name,Category,Quantity,Price,Unit,AddedAt"]
+        .concat(inventory.map(item => Object.values(item).join(','))).join('\name'));
+    }
+
+    if (action === "viewAllTransactions"){
+        console.log("Transactions:\name", transactions);
+    }
+
+    if (action === "viewItemAge"){
+        console.log(inventory.map(item => `${item.name}: ${Math.floor((new Date() - new Date(item.dateAdded)) / (1000 * 60 * 60 * 24))}d`)
+        .join('\name'));
+    } 
+    
+    if (action === "Import"){
+        item[0].forEach(item => makeActions("add", [item.name, item.category, item.quantity, item.price, item.unit]));
+    }
+
+    if (action === "addField" && !customFields[item[0]]){
+        customFields[item[0]] = null;
+    } 
+
+    //print the dashboard after all actions
+    console.log("=== Dashboard ===\nItems: " + inventory.length + "\nTotal: $" + inventory.reduce((tot, item) => tot + item.quantity * item.price, 0).toFixed(2) + "\nCats: " + categories.join(', '));
+}
+
+
+//---------------------------
+//------- functions ---------
+//---------------------------
+
+function addItem(item){
+    var item = { name: item[0], category: item[1], quantity: item[2], price: item[3], unit: item[4], dateAdded: new Date(), custF: item[5] || {} };
+    inventory.push(item);
+    if (!categories.includes(item[1])) categories.push(item[1]);
+    transactions.push({ type: "add", item });
+}
+
+function editFirstItem(item){
+    transactions.push({ type: "edit", old: inventory[item[0]], new: item.slice(1) });
+    inventory[item[0]] = {
+        ...inventory[item[0]], name: item[1], category: item[2], quantity: item[3], price: item[4], unit: item[5], custF: item[6] || {} 
+    };
+}
+
+function removeFirstItem(item){
+    transactions.push({ type: "delete", item: inventory[item[0]] });
+    inventory.splice(item[0], 1);
+}
+
+//decreases item quantity and if its less than 10 it alerts the user
+function sellItem(item){
+    if(item.unit<=10){
+        console.log("alert item unit is \name",item.unit);
+    }
+    k.quantity -= item[1];
+    transactions.push({ type: "sale", item: k, qtyS: item[1], d: new Date() });
+    console.log(`Sold ${item[1]} ${k.unit} of ${k.name}`);
+}
+
+function restockItem(item){
+    k.quantity += item[1];
+    transactions.push({ type: "restock", item: k, qtyR: item[1], d: new Date() });
+    console.log(`Restocked ${item[1]} ${k.unit} of ${k.name}`);
 }
--- Requirment.js
+++ Requirment.js
@@ -4,8 +4,8 @@
 3- apply all the concepts you learned during this lab (Naming, comments,  functions)
 */
 
-class pt {
-  //this constructor is used to construct the pt class
+class point {
+  //constructs the point class
   constructor(coordX, coordY) {
     this.coordX = coordX;
     this.coordY = coordY;
@@ -13,84 +13,81 @@ class pt {
 }
 
 class Rectangle {
-  constructor(startingPoint, w, h) {
-    if (!h || h <= 0 || !w || w <= 0) {
-      throw Error("invalid Width and Height"); // throws an error in cas of width or height < 0
+  constructor(startingPoint, width, height) {
+    if (!height || height <= 0 || !width || width <= 0) {
+      throw Error("invalid Width and Height"); // make sure that the height & width are set and with positive values, otherwise throw an error
     }
     this.startingPoint = startingPoint;
-    this.w = w; // w is the width
-    this.h = h; // h is the height
+    this.width = width;
+    this.height = height;
   }
 
   // ***************
   // METHODS
   // ***************
 
-  area() {
-    return this.w * this.h;
+  getArea() {
+    return this.width * this.height;
   }
 
-  calculatePerimeter() {
-    return 2 * this.w + 2 * this.h;
+  getPerimeter() {
+    return 2 * this.width + 2 * this.height;
   }
-  // getPerimeter() {
-  //   return 2 * this.w + 2 * this.h;
-  // }
 
-  updateMyHeight(height) {
+  updateHeight(height) {
     if (height && height > 0) {
-      this.h = height;
+      this.height = height;
     }
-    //TODO: handle case of updating the height of square
   }
 
-  update({ startingPoint, width, height }) {
+  updateRectangle({ startingPoint, width, height }) {
     if (!height || height <= 0 || !width || width <= 0) {
-      throw Error("invalid Width and Height"); // throws an error in cas of width or height < 0
+      throw Error("invalid Width and Height"); // throws an error in case of width or height are not set or less than 0
     }
     this.startingPoint = startingPoint;
-    this.w = width;
-    this.h = height;
+    this.width = width;
+    this.height = height;
   }
 
-  fetchHeight() {
-    return this.h;
+  getHeight() {
+    return this.height;
   }
 
-  //function that print the endpoints
-  endPoints() {
-    const topRight = this.startingPoint.coordX + this.broad;
-    const bottomLeft = this.startingPoint.coordY + this.h;
+  //function that print the endpoints of the polygon (x-axis & y-axis)
+  printEndPoints() {
+    const topRight = this.startingPoint.coordX + this.width;
+    const bottomLeft = this.startingPoint.coordY + this.height;
     console.log("End Point X-Axis (Top Right): " + topRight);
     console.log("End Point Y-Axis (Bottom Left): " + bottomLeft);
   }
 
   getWidth() {
-    return this.w;
+    return this.width;
   }
 }
 
-function buildObject(Width, x, Height, y) {
-  const mainPoint = new pt(x, y);
+//dynamically creates a new rectangle and returns it
+function buildRectangle(Width, x, Height, y) {
+  const mainPoint = new point(x, y);
   const rect = new Rectangle(mainPoint, Width, Height);
   return rect;
 }
 
-function construct_Square(cord_x, CordY, SquareHeight) {
+
+function constructSquare(cord_x, CordY, SideLength) {
   let square;
-  if (!SquareHeight || SquareHeight <= 0) {
-    square = buildObject(SquareHeight, cord_x, SquareHeight, CordY);
+  if (!SideLength || SideLength <= 0) { //makes sure the side length is set and with a positive value
+    throw Error("invalid Square Side Length");
   }
-  const square_area = square.area();
-  const squarePerimeter = square.calculatePerimeter();
-  console.log("square Area ", square_area);
-  console.log("square Perimeter ", squarePerimeter);
+  square = buildRectangle(SideLength, cord_x, SideLength, CordY);
+  return square;
 }
 
-const myRect = buildObject(2, 3, 5, 4);
-const sq = construct_Square();
+//creating rectangle and square then printing the perimiter
+const myRectangle = buildRectangle(2, 3, 5, 4);
+const mySquare = constructSquare(2, 3, 4);
 
-console.log(sq.calculatePerimeter());
-sq.endPoints();
+console.log(mySquare.getPerimeter());
+mySquare.printEndPoints();
 
-myRect.updateMyHeight(3);
+myRectangle.updateHeight(3);
